6fd8b64e0f9f3b3da9f68c521bc9385a702f23d6
Kaló Róbert <robert.kalo@gmail.com>
2017-11-16 15:42:33 +0100

    Update SimpleArraySum.java

--- src/com/hackerrank/main/algorithms/SimpleArraySum.java
+++ src/com/hackerrank/main/algorithms/SimpleArraySum.java
@@ -1,23 +1,19 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class SimpleArraySum {
 
-    static int simpleArraySum(int n, int[] ar) {
-        // Complete this function
-    }
+  static int simpleArraySum (int n, int[] ar){
+    return IntStream.of( ar ).sum();
+  }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] ar = new int[n];
-        for(int ar_i = 0; ar_i < n; ar_i++){
-            ar[ar_i] = in.nextInt();
-        }
-        int result = simpleArraySum(n, ar);
-        System.out.println(result);
-    }
+  public static void main (String[] args){
+    Scanner in = new Scanner( System.in );
+    int n = in.nextInt();
+    int[] ar = new int[n];
+    for (int ar_i = 0; ar_i < n; ar_i++)
+      ar[ar_i] = in.nextInt();
+    int result = simpleArraySum( n, ar );
+    System.out.println( result );
+  }
 }
